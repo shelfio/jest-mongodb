9931438b1922842f60cd2b862c78a09216892cc2
Vlad Holubiev <golubev.vld@gmail.com>
2018-02-15 02:15:25 +0200

    feat: add test example for insertMany

--- mongo-insert.test.js
+++ mongo-insert.test.js
@@ -15,8 +15,23 @@ afterAll(async () => {
 it('should insert a doc into collection', async () => {
   const users = db.collection('users');
 
-  await users.insertOne({_id: 'some-user-id', name: 'John'});
-  const insertedUser = await users.findOne({_id: 'some-user-id'});
+  const mockUser = {_id: 'some-user-id', name: 'John'};
+  await users.insertOne(mockUser);
 
-  expect(insertedUser).toEqual({});
+  const insertedUser = await users.findOne({_id: 'some-user-id'});
+  expect(insertedUser).toEqual(mockUser);
+});
+
+it('should insert many docs into collection', async () => {
+  const users = db.collection('users');
+
+  const mockUsers = [{name: 'Alice'}, {name: 'Bob'}];
+  await users.insertMany(mockUsers);
+
+  const insertedUsers = await users.find().toArray();
+  expect(insertedUsers).toEqual([
+    expect.objectContaining({name: 'John'}),
+    expect.objectContaining({name: 'Alice'}),
+    expect.objectContaining({name: 'Bob'})
+  ]);
 });
