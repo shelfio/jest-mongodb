3a2dd1591964bfbbf6c69ba2f49d4929827a7760
Vlad Holubiev <golubev.vld@gmail.com>
2018-02-15 02:21:51 +0200

    feat: add example test for aggregation

---
+++ mongo-aggregate.test.js
@@ -0,0 +1,36 @@
+const {MongoClient} = require('mongodb');
+
+let connection;
+let db;
+
+beforeAll(async () => {
+  connection = await MongoClient.connect(global.__MONGO_URI__);
+  db = await connection.db(global.__MONGO_DB_NAME__);
+});
+
+afterAll(async () => {
+  await db.close();
+});
+
+it('should aggregate docs from collection', async () => {
+  const files = db.collection('files');
+
+  await files.insertMany([
+    {type: 'Document'},
+    {type: 'Video'},
+    {type: 'Image'},
+    {type: 'Document'},
+    {type: 'Image'},
+    {type: 'Document'}
+  ]);
+
+  const topFiles = await files
+    .aggregate([{$group: {_id: '$type', count: {$sum: 1}}}, {$sort: {count: -1}}])
+    .toArray();
+
+  expect(topFiles).toEqual([
+    {_id: 'Document', count: 3},
+    {_id: 'Image', count: 2},
+    {_id: 'Video', count: 1}
+  ]);
+});
