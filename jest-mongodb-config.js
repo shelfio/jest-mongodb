c8aea74abca7182aba8b767480917914313c431a
Vlad Holubiev <golubev.vld@gmail.com>
2019-04-12 20:26:55 +0300

    feat: support jest-mongodb-config.js

---
+++ jest-mongodb-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mongodbMemoryServerOptions: {
+    instance: {
+      dbName: 'jest'
+    },
+    binary: {
+      version: '3.2.18'
+    },
+    autoStart: false
+  }
+};
--- setup.js
+++ setup.js
@@ -1,17 +1,10 @@
-const path = require('path');
 const fs = require('fs');
+const {resolve, join} = require('path');
+const cwd = require('cwd');
 const MongodbMemoryServer = require('mongodb-memory-server');
-const globalConfigPath = path.join(__dirname, 'globalConfig.json');
+const globalConfigPath = join(__dirname, 'globalConfig.json');
 
-const mongod = new MongodbMemoryServer.default({
-  instance: {
-    dbName: 'jest'
-  },
-  binary: {
-    version: '3.2.18'
-  },
-  autoStart: false
-});
+const mongod = new MongodbMemoryServer.default(getMongodbMemoryOptions());
 
 module.exports = async () => {
   if (!mongod.isRunning) {
@@ -31,3 +24,21 @@ module.exports = async () => {
   global.__MONGOD__ = mongod;
   process.env.MONGO_URL = mongoConfig.mongoUri;
 };
+
+function getMongodbMemoryOptions() {
+  try {
+    const {mongodbMemoryServerOptions} = require(resolve(cwd(), 'jest-mongodb-config.js'));
+
+    return mongodbMemoryServerOptions;
+  } catch (e) {
+    return {
+      instance: {
+        dbName: 'jest'
+      },
+      binary: {
+        version: '3.2.18'
+      },
+      autoStart: false
+    };
+  }
+}
