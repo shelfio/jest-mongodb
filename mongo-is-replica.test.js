320bd9335654d77ac358a6aa0df7839bc4d95483
Mason Rhodes <solaris765@users.noreply.github.com>
2020-06-04 15:25:26 -0700

    feat: added test to verify the mongo instance type

---
+++ mongo-is-replica.test.js
@@ -0,0 +1,31 @@
+const {MongoClient} = require('mongodb');
+
+describe('insert', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    db = await connection.db();
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  // Differs depending on which suite type of connection is used
+  // eslint-disable-next-line jest/no-if
+  it(`should ${process.env.TEST_REPLICASET ? 'not' : ''} connect to a replica set`, async () => {
+    const test_cmd = db.admin().command({replSetGetStatus: 1});
+
+    // eslint-disable-next-line jest/no-if
+    if (process.env.TEST_REPLICASET) {
+      const result = await test_cmd;
+
+      expect(result.members[0].stateStr).toBe('PRIMARY');
+    } else await expect(test_cmd).rejects.toThrow('not running with --replSet');
+  }, 30000);
+});
