0081058d7db79cea6176a3419ef350106c381a17
Greg Thornton <xdissent@me.com>
2021-02-20 07:37:19 -0600

    Remove global config in teardown

--- teardown.js
+++ teardown.js
@@ -1,6 +1,8 @@
 const debug = require('debug')('jest-mongodb:teardown');
+const globalConfig = require('./global-config');
 
 module.exports = async function () {
   debug('Teardown mongod');
   await global.__MONGOD__.stop();
+  await globalConfig.remove();
 };
