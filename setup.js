6a1d62ef15d13c5419e360e482bd8e4233fa4b45
Vlad Holubiev <golubev.vld@gmail.com>
2021-07-05 12:34:23 +0300

    fix: import 'mongodb-memory-server' properly

--- setup.js
+++ setup.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const {resolve, join} = require('path');
-const MongodbMemoryServer = require('mongodb-memory-server');
+const {MongoMemoryServer} = require('mongodb-memory-server');
 const cwd = process.cwd();
 
 const debug = require('debug')('jest-mongodb:setup');
-const mongod = new MongodbMemoryServer.default(getMongodbMemoryOptions());
+const mongod = new MongoMemoryServer(getMongodbMemoryOptions());
 
 const globalConfigPath = join(cwd, 'globalConfig.json');
 
