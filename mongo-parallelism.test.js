ccc71771f0e892ab199bdd042100711d8a9d9c4d
Siloss <scherbanvitali@gmail.com>
2021-09-16 12:49:17 +0300

    test: added tests that prove that each worker creates distinct mongo instance

---
+++ mongo-parallelism.test.js
@@ -0,0 +1,31 @@
+const {MongoClient} = require('mongodb');
+const {getSeparateMongoInstancesFlag} = require('./helpers');
+
+describe('parallelism: first worker', () => {
+  const uri = global.__MONGO_URI__;
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = await connection.db();
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  it('should have separate database', async () => {
+    const collection = db.collection('parallelism-test');
+
+    await collection.insertOne({a: 1});
+    const count = await collection.count({});
+
+    if (getSeparateMongoInstancesFlag()) {
+      expect(count).toBe(1);
+    }
+  });
+});
