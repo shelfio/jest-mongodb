22a3a5aa9765c543d2df98cfb6a863abd25ac94f
Özgün Yalcin <oezguen.yalcin@check24.de>
2022-06-30 16:53:01 +0200

    remove globalConfig file during teardown

--- teardown.js
+++ teardown.js
@@ -1,8 +1,21 @@
 const debug = require('debug')('jest-mongodb:teardown');
+const {join} = require('path');
+const fs = require('fs');
+
+const cwd = process.cwd();
+const globalConfigPath = join(cwd, 'globalConfig.json');
 
 module.exports = async function () {
   debug('Teardown mongod');
   if (global.__MONGOD__) {
     await global.__MONGOD__.stop();
   }
+  fs.unlink(globalConfigPath, err => {
+    if (err) {
+      debug('Config could not be deleted');
+
+      return;
+    }
+    debug('Config is deleted');
+  });
 };
